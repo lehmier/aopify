cfbffe946b2e82150594bc6942a10a9eb2211d8b
Jesse Lehmier <jesse@lehmier.com>
2015-06-20 12:58:57 -0700

    Add initial experiment and lib

---
+++ experiments/a0.js
@@ -0,0 +1,24 @@
+var aopify = function(fn) {
+  var wrapper = function() {
+
+    /* Invoke before-advice */
+    if (wrapper.before) {
+      wrapper.before.apply(this, arguments);
+    }
+
+    /* Invoke the original function */
+    console.log('About the invoke the original function');
+    var retVal = fn.apply(this, arguments);
+    console.log('Value returned by the original function:  ' + retVal);
+
+    /* Invoke after-advice */
+    if (wrapper.after) {
+      wrapper.after.apply(this, arguments);
+    }
+
+    return retVal;
+  };
+  return wrapper;
+};
+
+module.exports = aopify;
