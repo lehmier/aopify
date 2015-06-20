b4b7ba4b16c279b7628012ee55aeb0f7ab9ad269
Jesse Lehmier <jesse@lehmier.com>
2015-06-20 13:59:28 -0700

    Give after-advice access to value to be returned

---
+++ examples/after.js
@@ -0,0 +1,24 @@
+var aopify = require('../lib/index.js');
+
+/* Very generic string concat */
+var double = function(n) {
+  return n * 2;
+};
+console.log(double(6));
+
+/* aopify and log if the result is divisible by 3 */
+double = aopify(double);
+double.after = function(options) {
+  if (options.returning % 3 === 0) {
+    console.log(3 + ' divides ' + options.returning);
+  }
+};
+console.log(double(20));
+console.log(double(30));
+
+// Output:
+//
+// 12
+// 40
+// 3 divides 60
+// 60
