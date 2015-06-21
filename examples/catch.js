ec68a7ce765b3374869091b62aab012f43ca982f
Jesse Lehmier <jesse@lehmier.com>
2015-06-21 13:56:02 -0700

    Update catch to not return on error and pass error to after advice

--- examples/catch.js
+++ examples/catch.js
@@ -8,8 +8,18 @@ console.log(parseJson('{"x":3}'));
 
 /* aopify and wrap in a try-catch */
 parseJson = aopify(parseJson);
+
+/* Attach a catch handler */
 parseJson.catch = function(e) {
   console.log('[ERROR] ' + e);
+};
+
+/* Attach an after handler and inspect the error. */
+parseJson.after = function(options) {
+  console.log('About to return: ' + options.returning);
+  if (options.err) {
+    console.log('There was an error invoking this function.');
+  }
 };
 
 /* Parse malformed JSON. */
@@ -19,4 +29,6 @@ console.log(parseJson('{'));
 //
 // { x: 3 }
 // [ERROR] SyntaxError: Unexpected end of input
+// About to return: null
+// There was an error invoking this function.
 // null
